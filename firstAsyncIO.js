33075136b04165dc1b75a65456ca503024334634
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-18 16:39:35 -0400

    4th lesson in learnyounode

--- firstAsyncIO.js
+++ firstAsyncIO.js
@@ -1,7 +1,22 @@
 var fs = require('fs');
 var path = process.argv[2];
 var str = fs.readFile(path, 'utf8', function (error, data) {
-  console.log(data);
+  console.log(data.split('\n').length-1);
 });
-str = str.split('\n').length-1;
-console.log(str);
+
+/*
+official solution:
+var fs = require('fs')
+    var file = process.argv[2]
+
+    fs.readFile(file, function (err, contents) {
+      if (err) {
+        return console.log(err)
+      }
+      // fs.readFile(file, 'utf8', callback) can also be used
+      
+      var lines = contents.toString().split('\n').length - 1
+      console.log(lines)
+    })
+
+*/
