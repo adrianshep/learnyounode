4349bd2c78acb0372b44f0c39d8df8c51b270812
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-21 16:57:24 -0400

    6th lesson in learnyounode

--- modular.js
+++ modular.js
@@ -1,3 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 var myModule = require('./myModule.js');
+var dirName = process.argv[2];
+var fileExt = '.' + process.argv[3];
+fs.readdir(dirName, function(err, list) {
+  if (err) {
+    return console.log(err);
+  }
+  for (var i = 0; i < list.length; i++) {
+    if (path.extname(list[i]) == fileExt) {
+        console.log(list[i]);
+    }
+  }
+});
--- myModule.js
+++ myModule.js
@@ -1,2 +1,14 @@
 var fs = require('fs');
 var path = require('path');
+var dirName = process.argv[2];
+var fileExt = '.' + process.argv[3];
+fs.readdir(dirName, function(err, list) {
+  if (err) {
+    return console.log(err);
+  }
+  for (var i = 0; i < list.length; i++) {
+    if (path.extname(list[i]) == fileExt) {
+        console.log(list[i]);
+    }
+  }
+});
