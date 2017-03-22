0573265f45d12f2d75c0c26f1a7a576a648cd977
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-22 18:25:04 -0400

    6th lesson in learnyounode - 2nd file

--- myModule.js
+++ myModule.js
@@ -1,1 +1,12 @@
-
+module.exports = function myModule(dirName, fileExt, callback) {
+  fs.readdir(dirName, function(err, list) {
+    if (err) {
+      return console.log(err);
+    }
+    for (var i = 0; i < list.length; i++) {
+      if (path.extname(list[i]) == fileExt) {
+          console.log(list[i]);
+      }
+    }
+  });
+}
