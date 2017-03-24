c9b2bd74176c49d9054a30e0cabe494f0c3cef51
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-24 11:25:21 -0400

    6th lesson in learnyounode

--- modular.js
+++ modular.js
@@ -5,3 +5,18 @@ var fileName = process.argv[3];
 
 myModule(dirName, fileName, callback);
 console.log(file);
+
+
+/*
+
+var module = require('./module');
+
+module(process.argv[2], process.argv[3], function(err, data) {
+  if (err) console.error(err);
+
+  data.forEach(function(element) {
+    console.log(element);
+  });
+});
+
+*/
