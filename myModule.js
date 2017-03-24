80050927bb595a5a4ce7d2241d10eeb15c7c5803
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-24 11:50:43 -0400

    6th lesson in learnyounode - 2nd file

--- myModule.js
+++ myModule.js
@@ -1,33 +1,32 @@
-module.exports = function myModule(dirName, fileName, function bar (callback) {
-  foo (function (err, files) {
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (dirName, fileExt, callback) {
+  fs.readdir(dirName, function(err, files) {
     if (err) {
-      return callback (err);
+      return callback(err);
     }
-    callback (null, files);
-  })
-}) {
-  var fileExt = '.' + fileName;
-  fs.readdir (dirName, function(err, files) {
-    if (err) {
-      return console.log(err);
-    }
-    files.forEach(function (file) {
-      if (path.extname(file) === fileExt) {
-        return file;
+    callback(null, files);
+    list.forEach(function(d) {
+      if (path.extname(d) == '.' + fileExt) {
+        console.log(d);
       }
-    })
+    });
   });
 }
 
 /*
 
-var fs = require("fs");
+var fs = require('fs');
 var firstArg = process.argv[2], secondArg = process.argv[3];
 
 module.exports = function (firstArg, secondArg, callback) {
     fs.readdir(firstArg, function(err, data){
         if(err) return callback(err);
         callback(null, data);
+        list.forEach(function(d){
+        if (path.extname(d) == '.' +secondArg) console.log(d);
+    });
     });
 }
 
