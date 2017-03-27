5d7db7882121f3d9b3e3d181ccdcceb1173b405a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-27 15:37:54 -0400

    7th lesson in learnyounode

--- client.js
+++ client.js
@@ -1,1 +1,13 @@
 var url = process.argv[0];
+
+
+/*
+node documentation example:
+
+http.get("http://www.google.com/index.html", function(res) {
+  console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+*/
