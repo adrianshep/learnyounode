2cc468db3e54e6f6ffa619ab0b7b379a739d5c3f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-03-29 12:28:30 -0400

    8th lesson in learnyounode

--- collect.js
+++ collect.js
@@ -3,11 +3,10 @@ var url = process.argv[2];
 var bl = require('bl');
 var fs = require('fs');
 
-fs.createReadStream('README.md')
-  .pipe(bl(function (err, data) { // note 'new' isn't strictly required
-    // `data` is a complete Buffer object containing the full data
-    console.log(data.length);
-    console.log(data.toString());
+http.get(url).pipe(bl(function (err, data) {
+  console.log(data.length)
+  console.log(data.toString())
+}))
   })
 
 
