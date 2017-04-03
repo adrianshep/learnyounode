e118001825b206ff453d4157ce4cdb063a245e8c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-04-03 11:46:06 -0400

    11th lesson in learnyounode

--- fileServer.js
+++ fileServer.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.argv[2];
+var file = process.argv[3];
 
 var server = http.createServer(function (req, res) {
   // request handling logic...
@@ -11,7 +13,7 @@ var server = http.createServer(function (req, res) {
   });
   stream.pipe(response);
 });
-server.listen(8000);
+server.listen(port);
 
 
 
