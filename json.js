6401ce193126c78ae821cd6f513cd95a97218878
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-04-13 17:10:52 -0400

    13th and final lesson in learnyounode

--- json.js
+++ json.js
@@ -1,29 +1,41 @@
-var http = require('http')
-var url = require('url')
-var path = process.argv[2]
-
-var map = require('through2-map')
-
-var server = http.createServer(function (req, res) {
-
-  if (path === '/api/parsetime') {
-    return res.end();
-  } else {
-    
-  }
-
-  if (req.method !== 'POST') {
-      return res.end('send me a POST\n')
-      }
-
-      req.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase()
-      })).pipe(res)
-    })
-
-    server.listen(Number(process.argv[2]))
+var http = require('http');
+var url = require('url');
+var port = process.argv[2];
 
 
+function parsedUrl(url){
+	if(url.pathname === '/api/parsetime'){
+		var date = new Date(url.query.iso);
+		console.log(date);
+		return{
+			hour: date.getHours(),
+			minute: date.getMinutes(),
+			second: date.getSeconds()
+		};
+	}
+	else if (url.pathname === '/api/unixtime'){
+		return {
+			unixtime: new Date(url.query.iso).getTime()
+		};
+	}
+	else return null;
+}
+
+var server = http.createServer(function(req, res){
+	var urlPath = url.parse(req.url, true);
+	console.log(urlPath);
+	var resources = parsedUrl(urlPath);
+	console.log('resources are:'+resources);
+	res.end(JSON.stringify(resources));
+	console.log(JSON.stringify(resources));
+	if (resources) {
+		res.writeHead(200, { 'Content-Type': 'application/json'});
+	}
+		res.end(JSON.stringify(resources));
+
+})
+
+server.listen(port);
 
 
 /*
