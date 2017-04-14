dd75d619e4aa13494476f60f79deeda7be07adb7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-04-14 10:24:18 -0400

    13th and final lesson in learnyounode

--- json.js
+++ json.js
@@ -40,6 +40,45 @@ server.listen(port);
 
 /*
 
+official solution:
+
+var http = require('http')
+    var url = require('url')
+
+    function parsetime (time) {
+      return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+      }
+    }
+
+    function unixtime (time) {
+      return { unixtime: time.getTime() }
+    }
+
+    var server = http.createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url, true)
+      var time = new Date(parsedUrl.query.iso)
+      var result
+
+      if (/^\/api\/parsetime/.test(req.url)) {
+        result = parsetime(time)
+      } else if (/^\/api\/unixtime/.test(req.url)) {
+        result = unixtime(time)
+      }
+
+      if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+      } else {
+        res.writeHead(404)
+        res.end()
+      }
+    })
+    server.listen(Number(process.argv[2]))
+    
+
 ## HTTP JSON API SERVER (Exercise 13 of 13)
 
   Write an HTTP server that serves JSON data when it receives a GET request
